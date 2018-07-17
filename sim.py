312420e65f53f1cadb14a8c3519123a86a11473b
Luis Ulloa <ulloa@stanford.edu>
2018-07-16 19:15:25 -0700

    pythonic style

--- sim.py
+++ sim.py
@@ -10,36 +10,36 @@ from numpy import sin, cos, arccos, degrees
 # Calculate the similarity between the texts of three writers and an
 # unknown text in order to determine the most likely writer of that text.
 def main():
-    featureVec = createFeatureVec()
+    feature_vec = create_feature_vec()
 
-    f_mad = fileToString('madison.txt')
-    f_jj = fileToString('jj.txt')
-    f_ham = fileToString('hamilton.txt')
-    f_unk = fileToString('unknown.txt') # unknown.txt file was written by hamilton
+    f_mad = file_to_string('madison.txt')
+    f_jj = file_to_string('jj.txt')
+    f_ham = file_to_string('hamilton.txt')
+    f_unk = file_to_string('unknown.txt') # unknown.txt file was written by hamilton
 
-    madisonSim = getSimilarity(f_mad, f_unk, featureVec)
-    jjSim = getSimilarity(f_jj, f_unk, featureVec)
-    hamSim = getSimilarity(f_ham, f_unk, featureVec)
+    mad_sim = get_similarity(f_mad, f_unk, feature_vec)
+    jj_sim = get_similarity(f_jj, f_unk, feature_vec)
+    ham_sim = get_similarity(f_ham, f_unk, feature_vec)
 
-    plotSimilarity(madisonSim, jjSim, hamSim)
+    plot_similarity(mad_sim, jj_sim, ham_sim)
 
 # Uses numpy and matplotlib to plot four vectors based on their cosine angles (similarity).
 # The unknown vector is defined as the positive x-axis [0, 1] and the other vectors are
 # plotted with respect to that vector. Vector lengths are the same.
-def plotSimilarity(madisonSim, jjSim, hamSim):
+def plot_similarity(mad_sim, jj_sim, ham_sim):
     origin = [0, 0, 0, 0]
-    vecXs = [1, madisonSim, jjSim, hamSim]     # x = rcos(theta) = 1*similarity
-    vecYs = [0, sin(arccos(madisonSim)), sin(arccos(jjSim)), sin(arccos(hamSim))] # y=rsin(theta)=1*sin(cos^-1(similarity))
-    vecColors = ['r', 'y', 'g', 'b']
-    vecNames = ['Unknown Author', 'Madison', 'John Jay', 'Hamilton']
+    vecs_x = [1, mad_sim, jj_sim, ham_sim]     # x = rcos(theta) = 1*similarity
+    vecs_y = [0, sin(arccos(mad_sim)), sin(arccos(jj_sim)), sin(arccos(ham_sim))] # y=rsin(theta)=1*sin(cos^-1(similarity))
+    vec_colors = ['r', 'y', 'g', 'b']
+    vec_names = ['Unknown Author', 'Madison', 'John Jay', 'Hamilton']
 
     # plot each individual vector, for loop necessary since label can only take one argument
-    for origin, x, y, color, label in zip(origin, vecXs, vecYs, vecColors, vecNames):
-        if vecNames[0] not in label:
+    for origin, x, y, color, label in zip(origin, vecs_x, vecs_y, vec_colors, vec_names):
+        if vec_names[0] not in label:
             label = label + " with %.2f%% similarity and %.2f$^\circ$ angle difference" % (x*100, degrees(arccos(x)))
         plt.quiver(origin, origin, x, y, color=color,
                     label=label, angles='xy', scale_units='xy', scale=1)
-    plt.title('Cosine Similarity to ' + vecNames[0])
+    plt.title('Cosine Similarity to ' + vec_names[0])
     plt.legend(loc='upper left')
     plt.xlim(0, 1.5) # only [0, 1.0] is used, but need padding for labels
     plt.ylim(0, 1.5)
@@ -48,29 +48,23 @@ def plotSimilarity(madisonSim, jjSim, hamSim):
 # Count occurrences of a feature in text string by using the regex
 # findall function to isolate each word, which we compare to the feature
 def count_occurrences(text, feature):
-    counter = 0
-    for word in re.findall(r'\w+', text):
-        if word == feature:
-            counter += 1
-    return counter
+    found_occurrences = re.findall(r'\w+', text)
+    return len([word for word in found_occurrences if word == feature])
 
 # return a Vector (list) with the number of occurrences for each
 # feature in the corresponding index
-def createDocVec(text, featureVec):
-    result = []
-    for feature in featureVec:
-        result.append(count_occurrences(text, feature))
-    return result
+def create_doc_vec(text, feature_vec):
+    return [count_occurrences(text, feature) for feature in feature_vec]
 
 # Cosine Similarity, ternary used to catch divide by zero error
-def getSimilarity(text1, text2, featureVec):
-    docVec1 = createDocVec(text1, featureVec)
-    docVec2 = createDocVec(text2, featureVec)
-    dotP = np.dot(docVec1, docVec2)
-    return 0 if dotP == 0 else dotP / (norm(docVec1)*norm(docVec2))
+def get_similarity(text_1, text_2, feature_vec):
+    doc_vec_1 = create_doc_vec(text_1, feature_vec)
+    doc_vec_2 = create_doc_vec(text_2, feature_vec)
+    dot_p = np.dot(doc_vec_1, doc_vec_2)
+    return 0 if dot_p == 0 else dot_p / (norm(doc_vec_1)*norm(doc_vec_2))
 
 # Features that we are using to differentiate texts
-def createFeatureVec():
+def create_feature_vec():
     return ["a", "about", "above", "after", "again", "against", "all",
                 "am", "an", "and", "any", "are", "aren't", "as", "at", "be",
                 "because", "been", "before", "being", "below", "between", "both",
@@ -96,7 +90,7 @@ def createFeatureVec():
 # open: opens absolute path to filename, .join connects the following strings into a valid
 # dir path, __file__ is absolute path to sim.py, .dirname(__file__) removes /sim.py part
 # read caveat: have to remove \n's by replacing in order to properly read file to string
-def fileToString(filename):
+def file_to_string(filename):
     text = ""
     with open(os.path.join(os.path.dirname(__file__), 'texts', filename), 'r') as file:
         text=file.read().replace('\n', '')
