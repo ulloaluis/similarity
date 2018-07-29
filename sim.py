cd26a7718be88dfa54900254cc88f6a8c6e71f53
Luis Ulloa <ulloa@stanford.edu>
2018-07-29 03:10:38 -0700

    replaced comments with document strings

--- sim.py
+++ sim.py
@@ -7,9 +7,11 @@ import matplotlib.pyplot as plt
 from numpy.linalg import norm
 from numpy import sin, cos, arccos, degrees
 
-# Calculate the similarity between the texts of three writers and an
-# unknown text in order to determine the most likely writer of that text.
 def main():
+    """
+        Calculate the similarity between the texts of three writers and an
+        unknown text in order to determine the most likely writer of that text.
+    """
     feature_vec = create_feature_vec()
 
     f_mad = file_to_string('madison.txt')
@@ -23,10 +25,14 @@ def main():
 
     plot_similarity(mad_sim, jj_sim, ham_sim)
 
-# Uses numpy and matplotlib to plot four vectors based on their cosine angles (similarity).
-# The unknown vector is defined as the positive x-axis [0, 1] and the other vectors are
-# plotted with respect to that vector. Vector lengths are the same.
 def plot_similarity(mad_sim, jj_sim, ham_sim):
+    """
+        inputs: similarity values for madison, john jay, and hamilton
+        
+        Uses numpy and matplotlib to plot four vectors based on their cosine angles (similarity).
+        The unknown vector is defined as the positive x-axis [0, 1] and the other vectors are
+        plotted with respect to that vector. Vector lengths are the same.
+    """
     origin = [0, 0, 0, 0]
     vecs_x = [1, mad_sim, jj_sim, ham_sim]     # x = rcos(theta) = 1*similarity
     vecs_y = [0, sin(arccos(mad_sim)), sin(arccos(jj_sim)), sin(arccos(ham_sim))] # y=rsin(theta)=1*sin(cos^-1(similarity))
@@ -45,26 +51,38 @@ def plot_similarity(mad_sim, jj_sim, ham_sim):
     plt.ylim(0, 1.5)
     plt.show()
 
-# Count occurrences of a feature in text string by using the regex
-# findall function to isolate each word, which we compare to the feature
 def count_occurrences(text, feature):
+    """
+        inputs: feature that is searched for inside text
+        Count occurrences of a feature in text string by using the regex
+        findall function to isolate each word, which we compare to the feature
+    """
     found_occurrences = re.findall(r'\w+', text)
     return len([word for word in found_occurrences if word == feature])
 
-# return a Vector (list) with the number of occurrences for each
-# feature in the corresponding index
 def create_doc_vec(text, feature_vec):
+    """ 
+        inputs: feature_vec, which is the features to be searched for, 
+                and text, which is the text being searched
+        returns a vector (list) with the number of occurrences for each
+        feature in the corresponding index
+    """
     return [count_occurrences(text, feature) for feature in feature_vec]
 
-# Cosine Similarity, ternary used to catch divide by zero error
+# Cosine Similarity
 def get_similarity(text_1, text_2, feature_vec):
+    """ 
+        returns a similarity value between two texts by creating
+        vectors that document the occurrence of features in feature_vec
+        and then taking the dot products (ternary catches divide by zero)
+    """
     doc_vec_1 = create_doc_vec(text_1, feature_vec)
     doc_vec_2 = create_doc_vec(text_2, feature_vec)
     dot_p = np.dot(doc_vec_1, doc_vec_2)
     return 0 if dot_p == 0 else dot_p / (norm(doc_vec_1)*norm(doc_vec_2))
 
-# Features that we are using to differentiate texts
 def create_feature_vec():
+    """ Features that we are using to differentiate texts """
     return ["a", "about", "above", "after", "again", "against", "all",
                 "am", "an", "and", "any", "are", "aren't", "as", "at", "be",
                 "because", "been", "before", "being", "below", "between", "both",
@@ -87,10 +105,13 @@ def create_feature_vec():
                 "yourself", "yourselves", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".",
                 "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
 
-# open: opens absolute path to filename, .join connects the following strings into a valid
-# dir path, __file__ is absolute path to sim.py, .dirname(__file__) removes /sim.py part
-# read caveat: have to remove \n's by replacing in order to properly read file to string
 def file_to_string(filename):
+    """
+        inputs: file to be converted to string
+        open: opens absolute path to filename, .join connects the following strings into a valid
+        dir path, __file__ is absolute path to sim.py, .dirname(__file__) removes /sim.py part
+        read caveat: have to remove \n's by replacing in order to properly read file to string
+    """
     text = ""
     with open(os.path.join(os.path.dirname(__file__), 'texts', filename), 'r') as file:
         text=file.read().replace('\n', '')
@@ -99,4 +120,8 @@ def file_to_string(filename):
 # Keeps program from running when imported as a module in another program
 # Allows us to define main above the functions it requires, no function prototypes
 if __name__ == "__main__":
+    """
+        Keeps program from running when imported as a module in another program
+        Allows us to define main above the functions it requires, no function prototypes
+    """
     main()
